070ea6bbe76273dd3e524ce354fb41968a90a83d
Thomas <bandt@69grad.de>
2014-11-20 17:24:10 +0100

    Adds a static configuration file with a bcrypt encrypted password (admin/test).

---
+++ admin/config.js
@@ -0,0 +1,6 @@
+(function(config) {
+    config.credentials = {
+        userName: "admin",
+        hashedPassword: "$2a$10$l69mRxfKt2C3FV19vzSFbucJVn43cEE8mPwz9CAIdRxNsrpwb5Khi"
+    };
+}(module.exports));
