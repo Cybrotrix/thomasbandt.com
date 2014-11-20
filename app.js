733ab0d97c9e4cadf94f60253295dc70792ee96e
Thomas <bandt@69grad.de>
2014-11-20 15:37:38 +0100

    Hello World as a server ;-)

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+(function() {
+    "use strict";
+
+    var http = require("http");
+
+    var server = http.createServer(function(request, response) {
+        response.end("Hello World!");
+    });
+
+    server.listen(6969);
+
+}());
 console.log("Hello World");
