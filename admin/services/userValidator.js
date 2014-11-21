c2ee5416afbad7dbf6f734a98286873681286f74
Thomas <bandt@69grad.de>
2014-11-21 20:37:40 +0100

    Refactoring user validator + specs (no more mock data but actual user credentials).

--- admin/services/userValidator.js
+++ admin/services/userValidator.js
@@ -5,41 +5,26 @@
     var data = require("../data");
     var q = require("q");
 
-    var _adminUser;
-
-    module.init = function(adminUser) {
-        _adminUser = adminUser;
-    };
-
-    // TODO - refactor
-    module.init = function() {
-        var config = require("../config");
-        var User = require("../model/user");
-
-        _adminUser = new User(
-            config.credentials.userName,
-            config.credentials.hashedPassword
-        );
-    };
-
     module.validateLogin = function(userName, password) {
         var deferred = q.defer();
 
-        bcrypt.compare(password, _adminUser.hashedPassword, function(error, passwordIsvalid) {
-            var result = {};
+        data.users.getAdminUser().done(function(admin) {
+            bcrypt.compare(password, admin.hashedPassword, function(error, passwordIsvalid) {
+                var result = {};
 
-            if (!passwordIsvalid) {
-                result.errorMessage = "The password is not correct.";
-            } else if (userName !== _adminUser.userName) {
-                result.errorMessage = "The user name is not correct.";
-            }
+                if (!passwordIsvalid) {
+                    result.errorMessage = "The password is not correct.";
+                } else if (userName !== admin.userName) {
+                    result.errorMessage = "The user name is not correct.";
+                }
 
-            if (result.errorMessage === null) {
-                result.user = _adminUser;
-            }
+                if (result.errorMessage === undefined) {
+                    result.user = admin;
+                }
 
-            deferred.resolve(result);
-        });
+                deferred.resolve(result);
+            });
+        }, deferred.reject);
 
         return deferred.promise;
     };
--- admin/services/userValidatorSpec.js
+++ admin/services/userValidatorSpec.js
@@ -3,37 +3,31 @@
 
     var expect = require("chai").expect;
 
-    var User = require("../model/user");
-
     describe("UserValidator", function() {
         describe("When calling validateLogin", function() {
             var _userValidator;
 
             before(function() {
-                var hashedPassword = "$2a$10$l69mRxfKt2C3FV19vzSFbucJVn43cEE8mPwz9CAIdRxNsrpwb5Khi"; // = test
-                var mockAdminUser = new User("Max", hashedPassword);
-
                 _userValidator = require("./userValidator");
-                _userValidator.init(mockAdminUser);
             });
 
             it("returns an error message, when the user name is incorrect", function(done) {
-                _userValidator.validateLogin("Someone", "test").done(function(result) {
+                _userValidator.validateLogin("wrong_username", "test").done(function(result) {
                     expect(result.errorMessage).to.have.string("user name");
                     done();
                 }, done);
             });
 
             it("returns an error message, when the password is incorrect", function(done) {
-                _userValidator.validateLogin("Max", "wrong_password").done(function(result) {
+                _userValidator.validateLogin("admin", "wrong_password").done(function(result) {
                     expect(result.errorMessage).to.have.string("password");
                     done();
                 }, done);
             });
 
             it("returns the admin user, when the credentials are correct", function(done) {
-                _userValidator.validateLogin("Max", "test").done(function(result) {
-                    expect(result.user.userName).be.equal("Max");
+                _userValidator.validateLogin("admin", "test").done(function(result) {
+                    expect(result.user.userName).be.equal("admin");
                     done();
                 }, done);
             });
