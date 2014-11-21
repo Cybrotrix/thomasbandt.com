95b85eb7002839dc689e8135165923b0cd5d0a05
Thomas <bandt@69grad.de>
2014-11-21 17:09:32 +0100

    Organize all routes at one central place

--- admin/controllers/loginController.js
+++ admin/controllers/loginController.js
@@ -2,30 +2,31 @@
     "use strict";
 
     var passport = require("passport");
+    var routes = require("../routes");
 
     function authenticate(request, response, next) {
         if (request.isAuthenticated()) {
-            next()
+            next();
             return;
         }
 
         response.redirect("/admin/login");
-    };
+    }
 
     loginController.init = function(app, renderAdminView) {
-        app.get("/admin", authenticate, function(request, response) {
+        app.get(routes.index, authenticate, function(request, response) {
             renderAdminView(response, "index", {
                 userName: request.user.username
             });
         });
 
-        app.get("/admin/login", function(request, response) {
+        app.get(routes.login, function(request, response) {
             renderAdminView(response, "login", {
                 message: request.flash('error')
             });
         });
 
-        app.post('/admin/login',
+        app.post(routes.login,
             passport.authenticate('local', {
                 successRedirect: '/admin',
                 failureRedirect: '/admin',
@@ -33,7 +34,7 @@
             })
         );
 
-        app.get("/admin/logout", authenticate, function(request, response) {
+        app.get(routes.logout, authenticate, function(request, response) {
             request.logout();
             response.redirect("/admin/login");
         });
---
+++ admin/routes.js
@@ -0,0 +1,7 @@
+var routes = {
+    login: "/admin/login",
+    logout: "/admin/logout",
+    index: "/admin"
+};
+
+module.exports = routes;
