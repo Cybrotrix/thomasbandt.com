7c8ee9bfce6dde5ff76bf27b57bf9e2cdf3efe5a
Thomas <bandt@69grad.de>
2014-11-21 15:55:36 +0100

    Adds required modules for authentication and session as body-parser, cookie-parser, flash etc. and implements a simple login form (which does not use the user validator currently)

--- admin/controllers/loginController.js
+++ admin/controllers/loginController.js
@@ -1,11 +1,27 @@
 (function(loginController) {
     "use strict";
 
+    var passport = require("passport");
+
     loginController.init = function(app, renderAdminView) {
         app.get("/admin", function(request, response) {
             renderAdminView(response, "login", {
-                message: "Hello Blogger!"
+                message: request.flash('error')
             });
         });
+
+        app.get("/admin/login", function(request, response) {
+            renderAdminView(response, "login", {
+                message: request.flash('error')
+            });
+        });
+
+        app.post('/admin/login',
+            passport.authenticate('local', {
+                successRedirect: '/admin',
+                failureRedirect: '/admin',
+                failureFlash: true
+            })
+        );
     };
 }(module.exports));
--- app.js
+++ app.js
@@ -2,9 +2,33 @@
     "use strict";
 
     var app = require("express")();
+    var session = require("express-session");
+    var bodyParser = require("body-parser");
+    var cookieParser = require("cookie-parser");
 
-    app.engine('handlebars', require("express-handlebars")());
-    app.set('view engine', 'handlebars');
+    var passport = require("passport");
+    var LocalStrategy = require("passport-local").Strategy;
+
+    app.engine("handlebars", require("express-handlebars")());
+    app.set("view engine", "handlebars");
+
+    app.use(cookieParser());
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(session({
+        secret: "node-simple-blog",
+        resave: false,
+        saveUninitialized: true
+    }))
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(require('connect-flash')());
+
+    passport.use(new LocalStrategy(
+        function(username, password, done) {
+            return done(null, false, { message: "Keine Ahnung" });
+        }
+    ));
 
     var admin = require("./admin");
     admin.init(app);
