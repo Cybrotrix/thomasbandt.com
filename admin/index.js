879eb34651772388d290ff2ac5a986d60ad97350
Thomas <bandt@69grad.de>
2014-11-26 14:31:00 +0100

    Refactoring app.js

--- admin/index.js
+++ admin/index.js
@@ -4,10 +4,50 @@
     var controllers = require("./controllers");
 
     admin.init = function(app) {
+        setUpAuthentication(app);
+
         controllers.init(app);
     };
 
     admin.services = {
         userValidator: require("./services/userValidator")
     };
+
+    function setUpAuthentication(app) {
+        var passport = require("passport");
+        var LocalStrategy = require("passport-local").Strategy;
+
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        passport.use(new LocalStrategy(
+            function(username, password, done) {
+                admin.services.userValidator.validateLogin(username, password).done(
+                    function(result) {
+                        if (result.errorMessage) {
+                            done(null, false, { message: result.errorMessage });
+                        } else {
+                            done(null, result.user);
+                        }
+                    },
+                    function(error) {
+                        done(error)
+                    }
+                );
+            }
+        ));
+
+        passport.serializeUser(function(user, done) {
+            done(null, "admin");
+        });
+
+        passport.deserializeUser(function(user, done) {
+            done(null, "admin");
+        });
+
+        app.use(function(request, response, next) {
+            request.app.locals.isAuthenticated = request.isAuthenticated();
+            next();
+        });
+    }
 }(module.exports));
