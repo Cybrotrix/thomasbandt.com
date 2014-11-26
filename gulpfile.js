e8ba188b2c9e67edb23633b614d4901395974d52
Thomas <bandt@69grad.de>
2014-11-26 17:30:05 +0100

    Switching to mocha execution by gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var jshint = require("gulp-jshint");
 var gulp = require("gulp");
+var mocha = require('gulp-mocha');
 
-gulp.task("lint", function() {
+gulp.task("jshint", function() {
     return gulp.src([
         "*.js",
         "admin/**/*.js",
         "!admin/client/libraries/**/*.js"])
         .pipe(jshint())
         .pipe(jshint.reporter("default"));
+});
+
+gulp.task("mocha", function() {
+    return gulp.src([
+        "*Spec.js",
+        "admin/**/*Spec.js"])
+        .pipe(mocha({reporter: "spec"}));
 });
