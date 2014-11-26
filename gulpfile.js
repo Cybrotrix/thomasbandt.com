dd351619d13ebfa07cef1e539282d84c15e8feb7
Thomas <bandt@69grad.de>
2014-11-26 17:21:05 +0100

    Ignores client libraries

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var jshint = require("gulp-jshint");
 var gulp = require("gulp");
 
 gulp.task("lint", function() {
-    return gulp.src(["*.js", "admin/**/*.js"])
+    return gulp.src([
+        "*.js",
+        "admin/**/*.js",
+        "!admin/client/libraries/**/*.js"])
         .pipe(jshint())
         .pipe(jshint.reporter("default"));
 });
