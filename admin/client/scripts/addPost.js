a18a6f1a3d08e3a0bfa8614d2cec0159eef8e6af
Thomas <bandt@69grad.de>
2014-11-27 17:53:28 +0100

    Adds the addPostController (Angular)

---
+++ admin/client/scripts/addPost.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+
+    angular
+        .module("admin", [])
+        .controller("AddPostController", AddPostController);
+
+    AddPostController.$inject = ["$scope"];
+
+    function AddPostController($scope) {
+    }
+}());
