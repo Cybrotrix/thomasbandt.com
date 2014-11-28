a6a5d0398e82e6f40b0d23e539a16006d3bfe6fb
Thomas <bandt@69grad.de>
2014-11-28 13:03:27 +0100

    Renaming

--- admin/data/userRepositorySpec.js
+++ admin/data/userRepositorySpec.js
@@ -5,21 +5,21 @@
 
     describe("UserRepository", function() {
         describe("When calling getAdminUser with the correct credentials", function() {
-            var _userRepository;
+            var sut;
 
             before(function() {
-                _userRepository = require("./userRepository");
+                sut = require("./userRepository");
             });
 
             it("returns the admin user with its name", function(done) {
-                _userRepository.getAdminUser().done(function(user) {
+                sut.getAdminUser().done(function(user) {
                     expect(user.userName).to.be.equal("admin");
                     done();
                 }, done);
             });
 
             it("returns the admin user with its hashed password", function(done) {
-                _userRepository.getAdminUser().done(function(user) {
+                sut.getAdminUser().done(function(user) {
                     expect(user.hashedPassword.length).to.be.greaterThan(0);
                     done();
                 }, done);
