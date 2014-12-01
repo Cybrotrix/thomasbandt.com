4c87d7dd9e4baa32a9c1aa3c7ac8efda8ec5de98
Thomas <bandt@69grad.de>
2014-12-01 18:05:35 +0100

    Implements JS Bundling
    Bundles, minifies and uglifies all client-side scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var jshint = require("gulp-jshint"),
     gulp = require("gulp"),
-    mocha = require('gulp-mocha');
+    mocha = require("gulp-mocha"),
+    uglify = require("gulp-uglify"),
+    concat = require("gulp-concat"),
+    watch = require("gulp-watch");
 
 gulp.task("jshint", function() {
     return gulp.src([
@@ -19,4 +22,27 @@ gulp.task("mocha", function() {
         "data/**/*Spec.js",
         "admin/**/*Spec.js"])
         .pipe(mocha({reporter: "spec"}));
+});
+
+gulp.task("minify", function() {
+    var scriptsToBeMinified =
+    [
+        "admin/client/libraries/jquery/dist/jquery.min.js",
+        "admin/client/libraries/bootstrap/dist/js/bootstrap.min.js",
+        "admin/client/libraries/flat-ui/dist/js/flat-ui.min.js",
+        "admin/client/libraries/angular/angular.min.js",
+        "admin/client/libraries/angular-messages/angular-messages.min.js",
+        "admin/client/libraries/underscore/underscore-min.js",
+        "admin/client/libraries/markdown-js/dist/markdown.min.js",
+        "admin/client/libraries/bootstrap-markdown/js/bootstrap-markdown.js",
+        "admin/client/scripts/**/*.js"
+    ];
+
+    gulp.src(scriptsToBeMinified)
+        .pipe(watch(scriptsToBeMinified, function(files) {
+            return files
+                .pipe(uglify())
+                .pipe(concat("all.min.js"))
+                .pipe(gulp.dest("admin/client/dist"));
+        }));
 });
