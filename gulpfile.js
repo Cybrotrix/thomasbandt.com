fa4fb7ae34a34b48d0b13bc10422c749e0fda074
Thomas <bandt@69grad.de>
2014-12-01 21:28:22 +0100

    Switching to CSS bundling via Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var jshint = require("gulp-jshint"),
     mocha = require("gulp-mocha"),
     uglify = require("gulp-uglify"),
     concat = require("gulp-concat"),
-    gzip = require("gulp-gzip"),
+    sass = require("gulp-sass"),
+    minifyCSS = require("gulp-minify-css"),
     watch = require("gulp-watch");
 
 gulp.task("jshint", function() {
@@ -27,24 +28,38 @@ gulp.task("mocha", function() {
 
 gulp.task("minify", function() {
     var scriptsToBeMinified =
-    [
-        "admin/client/libraries/jquery/dist/jquery.min.js",
-        "admin/client/libraries/bootstrap/dist/js/bootstrap.min.js",
-        "admin/client/libraries/flat-ui/dist/js/flat-ui.min.js",
-        "admin/client/libraries/angular/angular.min.js",
-        "admin/client/libraries/angular-messages/angular-messages.min.js",
-        "admin/client/libraries/underscore/underscore-min.js",
-        "admin/client/libraries/markdown-js/dist/markdown.min.js",
-        "admin/client/libraries/bootstrap-markdown/js/bootstrap-markdown.js",
-        "admin/client/scripts/**/*.js"
-    ];
+        [
+            "admin/client/libraries/jquery/dist/jquery.min.js",
+            "admin/client/libraries/bootstrap/dist/js/bootstrap.min.js",
+            "admin/client/libraries/flat-ui/dist/js/flat-ui.min.js",
+            "admin/client/libraries/angular/angular.min.js",
+            "admin/client/libraries/angular-messages/angular-messages.min.js",
+            "admin/client/libraries/underscore/underscore-min.js",
+            "admin/client/libraries/markdown-js/dist/markdown.min.js",
+            "admin/client/libraries/bootstrap-markdown/js/bootstrap-markdown.js",
+            "admin/client/scripts/**/*.js"
+        ];
 
     gulp.src(scriptsToBeMinified)
-        .pipe(watch(scriptsToBeMinified, function(files) {
+        .pipe(watch(scriptsToBeMinified, function (files) {
             return files
                 .pipe(uglify())
                 .pipe(concat("all.min.js"))
-                .pipe(gzip({ append: false }))
+                .pipe(gulp.dest("admin/client/dist"));
+        }));
+
+    var stylesToBeMinified =
+        [
+            "admin/client/styles/**/*.scss",
+            "admin/client/styles/*.scss"
+        ];
+
+    gulp.src(stylesToBeMinified)
+        .pipe(watch(stylesToBeMinified, function(files) {
+            return files
+                .pipe(sass())
+                .pipe(concat("all.min.css"))
+                .pipe(minifyCSS())
                 .pipe(gulp.dest("admin/client/dist"));
         }));
 });
