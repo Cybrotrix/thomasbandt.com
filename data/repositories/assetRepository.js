361514b64241ab3e55e354c8f8201afa259a3aed
Thomas <bandt@69grad.de>
2014-12-02 21:14:49 +0100

    Making everything async

--- data/repositories/assetRepository.js
+++ data/repositories/assetRepository.js
@@ -11,20 +11,36 @@ module.exports = {
 function getAllFiles() {
     var deferred = q.defer();
 
-    fs.readdir(config.assets.uploadFolder, function(error, paths) {
-        var files = [];
+    fs.readdir(config.assets.uploadFolder, function(error, filePaths) {
+        var statPromises = [];
 
-        _.forEach(paths, function(path) {
-            var stats = fs.statSync(config.assets.uploadFolder + path);
-
-            files.push({
-                name: path.replace(/^.*[\\\/]/, ''),
-                size: stats.size,
-                created: stats.ctime
-            })
+        _.forEach(filePaths, function(filePath) {
+            statPromises.push(getFileStats(filePath));
         });
 
-        deferred.resolve(files);
+        q.allSettled(statPromises).done(function(results) {
+            var files = [];
+
+            results.forEach(function(result) {
+               files.push(result.value);
+            });
+
+            deferred.resolve(files);
+        });
+    });
+
+    return deferred.promise;
+}
+
+function getFileStats(filePath) {
+    var deferred = q.defer();
+
+    fs.stat(config.assets.uploadFolder + filePath, function(error, stats) {
+        deferred.resolve({
+            name: filePath,
+            size: stats.size,
+            created: stats.ctime
+        })
     });
 
     return deferred.promise;
