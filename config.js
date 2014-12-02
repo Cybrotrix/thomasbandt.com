6c2ca88df00c318be5fba9701958e2614261f070
Thomas <bandt@69grad.de>
2014-12-02 15:41:20 +0100

    Restructures config

--- config.js
+++ config.js
@@ -1,4 +1,17 @@
 var config = {
+    admin: {
+        credentials: {
+            userName: "admin",
+            hashedPassword: "$2a$10$l69mRxfKt2C3FV19vzSFbucJVn43cEE8mPwz9CAIdRxNsrpwb5Khi"
+        }
+    },
+    assets: {
+        uploadFolder: "/upload"
+    },
+    database: {
+        connectionString: "mongodb://localhost:27017/node-simple-blog",
+        databaseName: "node-simple-blog"
+    },
     debug: {
         database: {
             connectionString: "mongodb://localhost:27017/node-simple-blog-dev",
@@ -7,16 +20,6 @@ var config = {
     },
     metadata: {
         blogName: "Simpe Blog"
-    },
-    admin: {
-        credentials: {
-            userName: "admin",
-            hashedPassword: "$2a$10$l69mRxfKt2C3FV19vzSFbucJVn43cEE8mPwz9CAIdRxNsrpwb5Khi"
-        }
-    },
-    database: {
-        connectionString: "mongodb://localhost:27017/node-simple-blog",
-        databaseName: "node-simple-blog"
     }
 };
 
