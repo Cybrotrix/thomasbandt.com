5d6bad7bdb0225464061db96eec68959994aa134
Thomas <bandt@69grad.de>
2014-12-03 21:42:41 +0100

    Sorting all assets descending by upload date

--- admin/controllers/assets/assetsController.js
+++ admin/controllers/assets/assetsController.js
@@ -1,7 +1,8 @@
 var config = require("../../../config"),
     routes = require("../../../routes"),
     routeUtilities = require("../../utilities/routeUtilities"),
-    data = require("../../../data");
+    data = require("../../../data")
+    _ = require("underscore");
 
 module.exports = {
     init: init
@@ -10,8 +11,12 @@ module.exports = {
 function init(app) {
     app.get(routes.admin.assets, function(request, response) {
         data.assets.all().done(function(files) {
+            var sortedFiles = _.sortBy(files, function(file) {
+                return file.created * -1;
+            });
+
             routeUtilities.renderAdminView(response, "assets", {
-                files: files,
+                files: sortedFiles,
                 message: request.flash("asset-message")
             });
         });
