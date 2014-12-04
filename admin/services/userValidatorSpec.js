ae24978425aad8c4d51583e9959557858a22b5ab
Thomas <bandt@69grad.de>
2014-12-04 21:26:50 +0100

    renaming => sut

--- admin/services/userValidatorSpec.js
+++ admin/services/userValidatorSpec.js
@@ -2,28 +2,28 @@ var expect = require("chai").expect;
 
 describe("UserValidator", function() {
     describe("When calling validateLogin", function() {
-        var _userValidator;
+        var sut;
 
         before(function() {
-            _userValidator = require("./userValidator");
+            sut = require("./userValidator");
         });
 
         it("returns an error message, when the user name is incorrect", function(done) {
-            _userValidator.validateLogin("wrong_username", "test").done(function(result) {
+            sut.validateLogin("wrong_username", "test").done(function(result) {
                 expect(result.errorMessage).to.have.string("user name");
                 done();
             }, done);
         });
 
         it("returns an error message, when the password is incorrect", function(done) {
-            _userValidator.validateLogin("admin", "wrong_password").done(function(result) {
+            sut.validateLogin("admin", "wrong_password").done(function(result) {
                 expect(result.errorMessage).to.have.string("password");
                 done();
             }, done);
         });
 
         it("returns the admin user, when the credentials are correct", function(done) {
-            _userValidator.validateLogin("admin", "test").done(function(result) {
+            sut.validateLogin("admin", "test").done(function(result) {
                 expect(result.user.userName).be.equal("admin");
                 done();
             }, done);
