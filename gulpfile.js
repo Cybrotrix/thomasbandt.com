fa322d3d76d2ff384e5c6d4cbda97084b667856d
Thomas <bandt@69grad.de>
2014-12-05 22:22:43 +0100

    Updates some paths in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ gulp.task("jshint", function() {
     return gulp.src([
         "*.js",
         "admin/**/*.js",
+        "blog/**/*.js",
+        "!blog/client/libraries/**/*.js",
         "data/**/*.js",
         "!admin/client/dist/*.js",
         "!admin/client/libraries/**/*.js"])
@@ -25,7 +27,6 @@ gulp.task("mocha", function() {
         "*Spec.js",
         "data/**/*Spec.js",
         "blog/**/*Spec.js",
-        "utils/**/*Spec.js",
         "admin/**/*Spec.js"])
         .pipe(mocha({reporter: "spec"}));
 });
