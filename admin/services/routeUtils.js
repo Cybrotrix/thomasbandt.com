a7b7c11dc72f0b69d6d0837e26a37cb639d77c56
Thomas <bandt@69grad.de>
2014-12-06 13:47:40 +0100

    renames the slug method and adds another one

--- admin/services/routeUtils.js
+++ admin/services/routeUtils.js
@@ -1,20 +1,33 @@
 var slug = require("slug");
 
 module.exports = {
-    slug : getSlug
+    slugFromPost : getSlugFromPost,
+    slugFromRouteParameters : slugFromRouteParameters
 };
 
 slug.defaults.charmap['ä'] = 'ae';
 slug.defaults.charmap['ö'] = 'oe';
 slug.defaults.charmap['ü'] = 'ue';
 
-function getSlug(post) {
+function getSlugFromPost(post) {
     var slugParts =
         [
             post.date.getFullYear(),
             post.date.getMonth() + 1,
             post.date.getDate(),
             slug(post.title.toLowerCase())
+        ];
+
+    return slugParts.join("/");
+}
+
+function slugFromRouteParameters(parameters) {
+    var slugParts =
+        [
+            parameters.year,
+            parameters.month,
+            parameters.day,
+            parameters.title
         ];
 
     return slugParts.join("/");
