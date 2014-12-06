5aa08bdbba13dbaa70e291d1cb11c2e62242deda
Thomas <bandt@69grad.de>
2014-12-07 00:07:30 +0100

    Adds the info what password is used here

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
     admin: {
         credentials: {
             userName: "admin",
-            hashedPassword: "$2a$10$l69mRxfKt2C3FV19vzSFbucJVn43cEE8mPwz9CAIdRxNsrpwb5Khi"
+            hashedPassword: "$2a$10$l69mRxfKt2C3FV19vzSFbucJVn43cEE8mPwz9CAIdRxNsrpwb5Khi" // = test
         }
     },
     assets: {
