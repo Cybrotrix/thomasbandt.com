d4acb0d3b8c353b48b4f17f2897bbb859356a3e5
Thomas <bandt@69grad.de>
2014-12-22 13:01:01 +0100

    Introducing ifCond http://stackoverflow.com/a/16315366/271150

--- app.js
+++ app.js
@@ -66,6 +66,28 @@ function configureViewEngine(app) {
                 this.sections[name] = options.fn(this);
 
                 return null;
+            },
+            ifCond: function(v1, operator, v2, options) {
+                switch (operator) {
+                    case '==':
+                        return (v1 == v2) ? options.fn(this) : options.inverse(this);
+                    case '===':
+                        return (v1 === v2) ? options.fn(this) : options.inverse(this);
+                    case '<':
+                        return (v1 < v2) ? options.fn(this) : options.inverse(this);
+                    case '<=':
+                        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+                    case '>':
+                        return (v1 > v2) ? options.fn(this) : options.inverse(this);
+                    case '>=':
+                        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+                    case '&&':
+                        return (v1 && v2) ? options.fn(this) : options.inverse(this);
+                    case '||':
+                        return (v1 || v2) ? options.fn(this) : options.inverse(this);
+                    default:
+                        return options.inverse(this);
+                }
             }
         }
     });
