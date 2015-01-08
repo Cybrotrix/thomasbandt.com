db618f02102da06208365e2e50614435569575cc
Thomas <bandt@69grad.de>
2015-01-08 11:27:27 +0100

    Enables the upload folder to be located outside of the web root

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ function configureViewEngine(app) {
 }
 
 function configurePublicDirectories(app, express) {
-    app.use("/upload", express.static(__dirname + "/upload"));
+    app.use("/upload", express.static(config.assets.uploadFolder));
     app.use("/admin/client", express.static(__dirname + "/admin/client"));
     app.use("/blog/client", express.static(__dirname + "/blog/client"));
     app.use("/robots.txt", express.static(__dirname + "/robots.txt"));
