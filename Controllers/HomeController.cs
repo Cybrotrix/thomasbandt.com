1cf8761aa3f7c81782c62bbd7656445462ad3407
Thomas Bandt <bandt@69grad.de>
2017-01-27 22:03:53 +0100

    Changes routing and adds about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,14 @@ namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About()
         {
             return View();
         }
