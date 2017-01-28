ab7b1754b162c23678a39eaa3fb2c744e1356f3c
Thomas Bandt <bandt@69grad.de>
2017-01-28 21:33:08 +0100

    Brings all the styles back. Hi styles.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,45 +1,22 @@
-/// <binding Clean='clean' />
-"use strict";
+var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
+var cleanCss = require("gulp-clean-css");
+var rename = require("gulp-rename");
+var sass = require("gulp-sass");
 
-var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
-
-var webroot = "./wwwroot/";
-
-var paths = {
-    js: webroot + "js/**/*.js",
-    minJs: webroot + "js/**/*.min.js",
-    css: webroot + "css/**/*.css",
-    minCss: webroot + "css/**/*.min.css",
-    concatJsDest: webroot + "js/site.min.js",
-    concatCssDest: webroot + "css/site.min.css"
-};
-
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+gulp.task("styles", function() {
+    return gulp.src("./Client/Styles/Bundled.scss")
+        .pipe(sass())
+        .on("error", function(e) {
+            console.log(e);
+            this.emit("end");
+        })
+        .pipe(autoprefixer({ browsers: ["> 1%", "last 2 versions"] }))
+        .pipe(rename("blog.min.css"))
+        .pipe(cleanCss())
+        .pipe(gulp.dest("./wwwroot/styles/"));
 });
 
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
+gulp.task("default", ["styles"], function() {
+    gulp.watch("./Client/Styles/**/*.scss", ["styles"]);
 });
-
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
-});
-
-gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
-});
-
-gulp.task("min", ["min:js", "min:css"]);
