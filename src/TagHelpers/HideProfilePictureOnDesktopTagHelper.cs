919b21d073941f033f18261894ec10453188a987
Thomas Bandt <bandt@69grad.de>
2017-01-28 22:51:14 +0100

    Hides the picture on desktop on the about page

---
+++ src/TagHelpers/HideProfilePictureOnDesktopTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Blog.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "hideProfilePictureOnDesktop")]
+    public class HideProfilePictureOnDesktopTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            const string attributeName = "hideProfilePictureOnDesktop";
+
+            TagHelperAttribute attribute;
+            if (!context.AllAttributes.TryGetAttribute(attributeName, out attribute))
+            {
+                return;
+            }
+
+            output.Attributes.RemoveAll(attributeName);
+
+            if (!string.IsNullOrWhiteSpace(attribute.Value as string))
+            {
+                output.Attributes.Add("class", "hidden-on-desktop");
+            }
+        }
+    }
+}
