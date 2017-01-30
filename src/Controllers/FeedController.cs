8246bc9a45afe5d215d2c3047431a148d15e5f38
Thomas Bandt <bandt@69grad.de>
2017-01-30 21:05:08 +0100

    Implements the feed

--- src/Controllers/FeedController.cs
+++ src/Controllers/FeedController.cs
@@ -1,10 +1,18 @@
+using Blog.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class FeedController : Controller
     {
+        private readonly IPosts _posts;
+
+        public FeedController(IPosts posts)
+        {
+            _posts = posts;
+        }
+
         [Route("/feed")]
-        public IActionResult Feed() => Json("Here comes the RSS FEED some day.");
+        public IActionResult Feed() => View(_posts.Paged(1));
     }
 }
